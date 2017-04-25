51aa4c281fdd140b30adffd0e863e5188cd6e66d
Faldict <legadoubledyan@gmail.com>
2017-04-25 20:15:22 +0800

    backend api

--- controllers.go
+++ controllers.go
@@ -3,8 +3,41 @@ package main
 import (
     "net/http"
     "io"
+    "strings"
 )
 
 func helloController(w http.ResponseWriter, req *http.Request) {
-    io.WriteString(w, "I love U!\n")
+    io.WriteString(w, "Hello world!\n")
+}
+
+func userController(w http.ResponseWriter, req *http.Request) {
+    path := req.URL.EscapedPath()
+    method := strings.Split(path, "/")[2]
+
+    switch method {
+        case "login" :
+            loginUserController(w, req)
+        case "logout" :
+            logoutUserController(w, req)
+        case "info" :
+            infoUserController(w, req)
+        default:
+            NotFound(w, req)
+    }
+}
+
+func itemController(w http.ResponseWriter, req *http.Request) {
+    path := req.URL.EscapedPath()
+    method := strings.Split(path, "/")[2]
+
+    switch method {
+        case "add" :
+            addItemController(w http.ResponseWriter, req *http.Request)
+        case "list" :
+            listItemController(w http.ResponseWriter, req *http.Request)
+        case "share" :
+            shareItemController(w http.ResponseWriter, req *http.Request)
+        default:
+            NotFound(w, req)
+    }
 }
