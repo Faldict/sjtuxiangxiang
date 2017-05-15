82a7029727d01084d48f06bd3c09660bc7635920
Faldict <legadoubledyan@gmail.com>
2017-05-15 09:20:24 +0800

    false add item
    伪后端实现，增加了modal的关闭

--- static/js/example.js
+++ static/js/example.js
@@ -1,7 +1,28 @@
 function addItem() {
     var number = document.getElementsByClassName('number').length + 1;
+    var myForm = document.forms[1];
+    var title = myForm["request-title"].value;
+    var price = myForm["request-price"].value;
+    var perday = myForm["request-days"].value;
+    var pertime = myForm["request-times"].value;
+    var unit;
+    if (perday) {
+        unit = price + '元／天';
+    } else if (pertime) {
+        unit = price + '元/次';
+    } else {
+        unit = '待商定';
+    }
+    var xieshang = myForm["request-xieshang"].value;
+    var content = myForm["request-content"].value;
+    var date1 = document.getElementById('dtp_input1').value;
+    var date2 = document.getElementById('dtp_input2').value;
     var item = document.createElement("tr");
     item.innerHTML = '<td><span class="label label-default">未完成</span></td><td class="number">' + number +
-    '</td><td>求借单反相机进行社团活动拍摄</td><td>2017.1.1~2017.1.2</td><td>admin</td><td>50元/天</td>';
+                '</td><td>' + title +
+                '</td><td>' + date1 + '至' + date2 +
+                '</td><td>admin</td><td>' + unit + '</td>';
     document.getElementById('table-body').appendChild(item);
+    myForm.reset();
+    $('#uploadrequest').modal('hide');
 }
